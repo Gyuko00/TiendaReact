c75b87039125767eeea9e81c71e1bf8291411537
Gyuko00 <josuedhernandez@uts.edu.co>
2025-03-19 10:04:50 -0500

    refactor: move cart logic to CartContext provider and add localStorage persistence

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,17 @@
 import "./helpers/css/main.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import Navbar from "./components/nav/Navbar";
 import ItemListContainer from "./components/items/ItemListContainer";
 import ItemDetailContainer from "./components/items/ItemDetailContainer";
 import AboutUs from "./components/about-us/AboutUs";
 import Contact from "./components/contact/Contact";
 import Carrito from "./components/carrito/Carrito";
-import { CartContext } from "./context/CartContext";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
-  const [carrito, setCarrito] = useState([]);
-
-  const agregarAlCarrito = (item, cantidad) => {
-    const itemAgregado = { ...item, cantidad };
-
-    const nuevoCarrito = [...carrito];
-
-    const estaEnElCarrito = nuevoCarrito.find(
-      (producto) => producto.id === itemAgregado.id
-    );
-
-    if (estaEnElCarrito) {
-      estaEnElCarrito.cantidad += cantidad;
-      setCarrito(nuevoCarrito);
-    } else {
-      nuevoCarrito.push(itemAgregado);
-    }
-    setCarrito(nuevoCarrito);
-  };
-
-  const cantidadCarrito = () => {
-    return carrito.reduce((acc, producto) => acc + producto.cantidad, 0)
-  }
-
-  const precioTotal = () => {
-    return carrito.reduce((acc, producto) => acc + producto.precio * producto.cantidad, 0)
-  }
-
-  const vaciarCarrito = () => {
-    setCarrito([])
-  }
-
   return (
     <div>
-      <CartContext.Provider value={{ carrito, agregarAlCarrito, cantidadCarrito, precioTotal, vaciarCarrito }}>
+      <CartProvider>
         <BrowserRouter>
 
           <Navbar />
@@ -56,10 +23,11 @@ function App() {
             <Route path="/products/:category" element={<ItemListContainer />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Carrito />}/>
+            <Route path="/cart" element={<Carrito />} />
           </Routes>
+          
         </BrowserRouter>
-      </CartContext.Provider>
+      </CartProvider>
     </div>
   );
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,55 @@
-import { createContext } from "react";
-
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
+
+const carritoInicial = JSON.parse(localStorage.getItem("carrito")) || [];
+
+export const CartProvider = ({ children }) => {
+  const [carrito, setCarrito] = useState(carritoInicial);
+
+  const agregarAlCarrito = (item, cantidad) => {
+    const itemAgregado = { ...item, cantidad };
+
+    const nuevoCarrito = [...carrito];
+
+    const estaEnElCarrito = nuevoCarrito.find(
+      (producto) => producto.id === itemAgregado.id
+    );
+
+    if (estaEnElCarrito) {
+      estaEnElCarrito.cantidad += cantidad;
+      setCarrito(nuevoCarrito);
+    } else {
+      nuevoCarrito.push(itemAgregado);
+    }
+    setCarrito(nuevoCarrito);
+  };
+
+  const cantidadCarrito = () => {
+    return carrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+  };
+
+  const precioTotal = () => {
+    return carrito.reduce(
+      (acc, producto) => acc + producto.precio * producto.cantidad,
+      0
+    );
+  };
+
+  const vaciarCarrito = () => {
+    setCarrito([]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+  }, [carrito])
+
+  return (
+    <CartContext.Provider value={{
+         carrito, agregarAlCarrito, cantidadCarrito, precioTotal, vaciarCarrito 
+    }}>
+        { children }
+    </CartContext.Provider>
+  )
+
+};
